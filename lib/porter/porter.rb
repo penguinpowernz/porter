dd91c44a8fa0b63e7c90a6de5bb02df7cfec1b0f
Robert <hamstar@telescum.co.nz>
2013-02-17 03:20:53 +1300

    implemented preparation of porter

--- lib/porter/porter.rb
+++ lib/porter/porter.rb
@@ -5,7 +5,20 @@ module Porter
   class << self
   
     def prepare(red, green)
-    
+      
+      # Turn on forwarding in the kernel
+      system "/sbin/sysctl net.ipv4.ip_forward=1"
+      
+      # Allow forwarding over the given interfaces
+      [red]+green.split(",").each do |if|
+        if File.exist? "/proc/sys/net/ipv4/conf/#{if}/forwarding"
+          system "echo 1 > /proc/sys/net/ipv4/conf/#{if}/forwarding"
+        end
+      end
+      
+      # TODO: Save settings to config file
+      Porter.interfaces("red", red)
+      Porter.interfaces("green", green)
     end
     
     def add(red_port, green_host, green_port)
