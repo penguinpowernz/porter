45e9a48f9e3f95bd0fd435b7128cf50f6f5d9982
Robert <hamstar@telescum.co.nz>
2013-02-17 02:37:35 +1300

    really fixed indenting this time

--- lib/porter/porter.rb
+++ lib/porter/porter.rb
@@ -7,41 +7,40 @@ module Porter
     def prepare(red, green)
     
     end
-  
-  	def add(red_port, green_host, green_port)
+    
+    def add(red_port, green_host, green_port)
       Porter::PortForward.new(
-      	red_port, 
-      	green_host, 
-      	green_port
+        red_port, 
+        green_host, 
+        green_port
       ).enable( Porter::IpTables )
-      
-  	end
-
-  	def delete(red_port)
+    end
+    
+    def delete(red_port)
       Porter::PortForward.find( red_port ).disable( Porter::IpTables )
-  	end
-
-  	def list
-  	  puts "Red Port\tGreen Host\tGreen Port"
-  	  Porter::PortForward.all.each do |pf|
-  	  	puts "#{pf.red_port}\t#{pf.green_host}\t#{pf.green_port}"
-  	  end
-  	end
+    end
+    
+    def list
+      puts "Red Port\tGreen Host\tGreen Port"
+      Porter::PortForward.all.each do |pf|
+        puts "#{pf.red_port}\t#{pf.green_host}\t#{pf.green_port}"
+      end
+    end
     
     def interfaces
     
     end
-
-  	def disable_all
+    
+    def disable_all
       Porter::PortForward.all.each do |pf|
         pf.disable Porter::IpTables
       end
-  	end
-
-  	def enable_all
+    end
+    
+    def enable_all
       Porter::PortForward.all.each do |pf|
         pf.enable Porter::IpTables
       end
-  	end
+    end
   end
 end
