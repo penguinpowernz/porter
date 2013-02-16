84c747c2297f7dacd897266a589cf8f9b578afea
Robert <hamstar@telescum.co.nz>
2013-02-17 00:41:22 +1300

    implemented run and made it raise error on failure

--- lib/porter/ip_tables.rb
+++ lib/porter/ip_tables.rb
@@ -1,7 +1,9 @@
 module Porter
+  class IpTablesRuleError < StandardError; end
   class IpTables
-    def run
-
+    def run(rule)
+      output = `/sbin/iptables #{rule}`
+      raise IpTablesRuleError, output if $?.exitstatus != 0
     end
   end
 end
