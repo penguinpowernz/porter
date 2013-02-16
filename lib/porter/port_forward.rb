d68f452af1ed77e65faf1cb4a2dbbcaee6b77252
Robert <hamstar@telescum.co.nz>
2013-02-16 22:51:47 +1300

    added rules to port foward

--- lib/porter/port_forward.rb
+++ lib/porter/port_forward.rb
@@ -13,6 +13,16 @@ module Porter
       self.new *line.split(" ")
     end
 
+    def enable(iptables)
+      rules = [
+        "-A PREROUTING -t nat -i eth1 -p tcp --dport #{@local_port} -j DNAT --to #{@remote_host}:#{@remote_port}",
+        "-A PREROUTING -t nat -i eth1 -p udp --dport #{@local_port} -j DNAT --to #{@remote_host}:#{@remote_port}",
+        "-A INPUT -p tcp -m state --state NEW --dport #{@local_port} -i eth1 -j ACCEPT"
+      ]
+
+      rules.each do |rule|
+        iptables.run rule
+      end
     end
 
     def disable
