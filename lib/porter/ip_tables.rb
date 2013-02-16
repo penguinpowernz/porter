1dc5595882a006282066bc1d0a45bd861a38819d
Robert <hamstar@telescum.co.nz>
2013-02-17 04:26:53 +1300

    added methods for deleting and adding rules

--- lib/porter/ip_tables.rb
+++ lib/porter/ip_tables.rb
@@ -5,5 +5,13 @@ module Porter
       output = `/sbin/iptables #{rule}`
       raise IpTablesRuleError, output if $?.exitstatus != 0
     end
+    
+    def delete(rule)
+      run("-D #{rule}")
+    end
+    
+    def add(rule)
+      run("-A #{rule}")
+    end
   end
 end
