8de14f3ea3826699b39745494c40e02e9d1890b2
Robert <hamstar@telescum.co.nz>
2013-02-17 03:48:27 +1300

    added some horrible code for configuring porter

--- lib/porter.rb
+++ lib/porter.rb
@@ -1,8 +1,43 @@
+require 'yaml'
+
 module Porter
 
   class << self
     def store
       "/var/cache/porter/store.txt"
+    end
+    
+    def config_file
+      "/etc/porter/config.yaml"
+    end
+    
+    # Todo: refactor all the shit under this line
+    
+    def config
+      YAML::load_file(Porter.config_file ) if c.nil?
+    end
+    
+    def config=(c)
+      File.open(Porter.config_file, "w") {|f| f.write c.to_yaml}
+    end
+    
+    def interfaces(type=nil, new_value=nil)
+      
+      raise ArgumentError if type != "green" or type != "red"
+      
+      if !new_value.nil?
+        c = Porter.config
+        c["porter"][:interfaces][type.to_sym] = new_value
+        Porter.config = c
+      end
+      
+      if type.nil?
+        ifs << *Porter.config["porter"][:interfaces][:green].split(",")
+        ifs << Porter.config["porter"][:interfaces][:red]
+        return ifs
+      end
+      
+      Porter.config["porter"][:interfaces][type.to_sym]
     end
   end
 
